4064f5e6fd55332c4b3e16f46162608933370fb6
emanoelyaraujo <emanoelyaraujo004@gmail.com>
2021-10-08 17:56:55 -0300

    feat{comuns}: criação dos arquivos comuns

---
+++ site/comuns/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Sidebars · Bootstrap v5.0</title>
+
+    <script src="https://unpkg.com/feather-icons"></script>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    </head>
+
+    <style>
+    .btn:focus {
+        outline: none;
+        box-shadow: none;
+    }
+    </style>
+
+<body>
