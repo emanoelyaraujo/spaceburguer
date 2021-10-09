7763fc5e730def84598b000ddc32e55d766d043f
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-10-09 17:51:34 -0300

    feat: views Login e Cadastrar

--- site/cadastrar.php
+++ site/cadastrar.php
@@ -1,45 +1,63 @@
-<div class="container">
-
-    <div class="row">
-        <div class=" col-md-6 offset-md-3 shadow-lg p-1 login">
-            <form class="row align-self-center" action="#" method="post">
-
-                <div class="form-group col-md-6">
-                    <svg class="bi" ... role="img" aria-label="Tools">
-                        <use xlink:href="bootstrap-icons.svg#tools" />
-                    </svg>
+<main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+    <div class="container">
+        <div class="login-card">
+            <div class="row no-gutters">
+                <div class="col-md-5 area-img">
+                    <img class="login-card-img" id="pacote" src="<?= SITE_URL ?>assets/img/pacote.jpg" width="100%" height="100%" alt="">
                 </div>
-
-                <div class="form-group col-md-12 mt-3">
-                    <label for="input-group">Nome</label>
-                    <input type="text" class="form-control" name="nome" id="nome" placeholder="Ex: João de Souza" required>
+                <div class="col-md-7">
+                    <div class="card card-body">
+                        <form class="p-3">
+                            <h3>Cadastro</h3>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <div class="form-group">
+                                        <label for="nome">Nome<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" maxlength="100" id="nome" autofocus required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <div class="form-group">
+                                        <label for="telefone">Telefone<span class="text-danger">*</span></label>
+                                        <input type="text" maxlength="11" class="form-control" id="telefone" autofocus required>
+                                    </div>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <div class="form-group">
+                                        <label for="email">E-mail<span class="text-danger">*</span></label>
+                                        <input type="email" maxlength="100" class="form-control" id="email" aria-describedby="emailHelp" autofocus required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <div class="form-group">
+                                        <label for="password">Senha<span class="text-danger">*</span></label>
+                                        <input type="password" maxlength="15" class="form-control" id="password" id="password" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <div class="form-group">
+                                        <label for="confirmPassword">Confirme sua senha<span class="text-danger">*</span></label>
+                                        <input type="password" maxlength="15" class="form-control" id="confirmPassword" id="confirmPassword" required>
+                                    </div>
+                                </div>
+                                <div class="form-group mt-2 mb-3">
+                                    <input type="checkbox" id="f-option2" name="selector">
+                                    <label for="f-option2">Mantenha-me conectado</label>
+                                </div>
+                                <div class="d-flex justify-content-center">
+                                    <div class="form-group">
+                                        <a class="btn btn-outline-secondary" href="<?= SITE_URL ?>" title="Voltar" id="voltar">Voltar</a>
+                                        <button class="btn btn-primary" type="submit" id="entrar" title="Cadastrar">Cadastrar</button>
+                                    </div>
+                                </div>
+                                <div class="linksLogin text-center mt-3 mb-4">
+                                    <a href="<?= SITE_URL ?>login">Já possui conta? Faça seu login</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-
-                <div class="form-group col-md-12 mt-3">
-                    <label for="input-group">Email</label>
-                    <input type="email" class="form-control" name="emai" id="email" placeholder="exemplo@exemplo.com" required>
-                </div>
-
-                <div class="form-group col-md-6 mt-3">
-                    <label for="input-group">Password</label>
-                    <input type="password" class="form-control" name="password" id="password" placeholder="********" required>
-                </div>
-
-                <div class="form-group col-md-6 mt-3">
-                    <label for="input-group">Confirme seu password</label>
-                    <input type="password" class="form-control" name="confirmPassword" id="confirmPassword" placeholder="********" required>
-                </div>
-
-                <div class="form-group col-md-12 mt-3">
-                    <label for="input-group">Foto perfil</label>
-                    <input type="text" class="form-control" name="linkFoto" id="linkFoto" placeholder="Insera o link da imagem para carrega-la">
-                </div>
-
-                <div class="col-md-12 novaConta text-center mt-4">
-                    <a type="submit" class="btn btn-primary" href="<?=SITE_URL?>login">Cadastar</a>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
-
-</div>
+</main>
--- site/login.php
+++ site/login.php
@@ -1,34 +1,40 @@
-<div class="container">
-
-    <div class="row">
-        <div class=" col-md-6 offset-md-3 shadow-lg login">
-            <form class="row align-self-center" action="#" method="post">
-
-                <div class="form-group col-md-6">
-                    <svg class="bi" ... role="img" aria-label="Tools">
-                        <use xlink:href="bootstrap-icons.svg#tools" />
-                    </svg>
+<main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+    <div class="container">
+        <div class="login-card">
+            <div class="row no-gutters">
+                <div class="col-md-5 area-img">
+                    <img class="login-card-img" id="pacote" src="<?= SITE_URL ?>assets/img/pacote.jpg" width="100%" height="100%" alt="">
                 </div>
-
-                <div class="form-group col-md-12">
-                    <label for="input-group">Usuário</label>
-                    <input type="email" class="form-control" name="usuario" id="email" placeholder="exemplo@teste.com" required>
+                <div class="col-md-7">
+                    <div class="card card-body">
+                        <form method="post" action="" class="row justify-content-center align-items-center">
+                            <div class="col-lg-6">
+                                <h3>Login</h3>
+                                <div class="col-md-12 form-group mb-3">
+                                    <label for="email" class="form-label">E-mail<span class="text-danger">*</span></label>
+                                    <input type="email" name="email" class="form-control" id="email" required>
+                                </div>
+                                <div class="col-md-12 form-group mb-3">
+                                    <label for="senha" class="form-label">Senha<span class="text-danger">*</span></label>
+                                    <input type="senha" name="senha" class="form-control" id="senha" required>
+                                </div>
+                                <div class="form-group mt-2 mb-3">
+                                    <input type="checkbox" id="f-option2" name="selector">
+                                    <label for="f-option2">Mantenha-me conectado</label>
+                                </div>
+                                <div class="col-6 mx-auto">
+                                    <a href="<?= SITE_URL ?>home" type="submit" class="btn btn-outline-secondary">Voltar</a>
+                                    <button type="submit" class="btn btn-primary">Entrar</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="text-center mt-3">
+                            <a href="">Esqueci minha senha</a><br>
+                            <a href="<?= SITE_URL ?>cadastrar">Novo no site? Cadastre-se</a>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="form-group col-md-12 mt-3">
-                <label for="input-group">Senha</label>
-                    <input type="password" class="form-control" name="password" id="senha" placeholder="********" required>
-                </div>
-
-                <div class="col-md-12 offset-md-5">
-                    <a type="submit" href="#" class="btn btn-primary mt-3">Login</a>
-                </div>
-                <div class="col-md-12 novaConta text-center mt-4">
-                    <a type="submit" href="<?=SITE_URL?>cadastrar">Criar minha conta</a>
-                </div>
-
-            </form>
+            </div>
         </div>
     </div>
-
-</div>
+</main>
