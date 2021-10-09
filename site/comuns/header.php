7113ca650a0042af910125604b993bba17e93aa2
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-10-09 17:56:16 -0300

    feat{header}: link CSS e script JS

--- site/comuns/header.php
+++ site/comuns/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="pt-br">
 
-    <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -10,13 +10,9 @@
     <title>Sidebars · Bootstrap v5.0</title>
 
     <script src="https://unpkg.com/feather-icons"></script>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        </head>
-    <link href="<?=SITE_URL?>assets/css/style.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="<?= SITE_URL ?>assets/css/bootstrap.min.css">
+    <script src="<?= SITE_URL ?>assets/js/bootstrap.min.js"></script>
+    <link href="<?= SITE_URL ?>assets/css/style.css" rel="stylesheet">
+</head>
 
 <body>
