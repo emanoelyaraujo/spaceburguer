8ed408f423f2e44dac4243c043d45badea5abe78
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-10-09 17:50:21 -0300

    feat: configurações da base de dados

--- config/config.php
+++ config/config.php
@@ -4,3 +4,12 @@
 
     //define("SITE_URL","http://netto-blog/");
     define("SITE_URL", "http://{$_SERVER['HTTP_HOST']}/");
+
+    // Defie constantes com configurações de conexão com a base de dados
+
+    define("DB_TYPE"    , 'mysql');
+    define('DB_HOST'    , 'localhost');
+    define('DB_PORT'    , '3306');
+    define('DB_USER'    , 'root');
+    define('DB_PASSWORD', '');
+    define("DB_BDADOS"  , 'spaceburguer');
