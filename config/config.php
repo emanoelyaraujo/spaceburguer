ee9dd2c093351c7a628daf687f1c796c226dd7c7
RomarioOliveira21 <87107730+RomarioOliveira21@users.noreply.github.com>
2021-10-08 21:13:16 -0300

    feat - arquivos de configuração e CSS e paginas do site

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+    // endereço (url), base de nossa aplicação web
+
+    //define("SITE_URL","http://netto-blog/");
+    define("SITE_URL", "http://{$_SERVER['HTTP_HOST']}/");
---
+++ site/comuns/error.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="alert alert-danger d-flex alert-dismissible" role="alert">
+        <div>
+            <strong>Erro! </strong>Não encontramos a página solicitada.
+        </div>
+        <div class="col-1">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+</div>
