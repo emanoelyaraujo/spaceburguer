9c9e382b185b00c2b77e1e37b4734bb663660072
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-10-11 20:10:56 -0300

    Revert "feat{Sidebar}: alterar a visualização conforme o nível do usuário e um ícone para representar o usuário que está logado"
    This reverts commit 9b3bbd2ad51b609670d816ed54b30e49f08e196d.

--- site/comuns/sidebar.php
+++ site/comuns/sidebar.php
@@ -1,6 +1,3 @@
-<!-- <?php
-var_dump($_SESSION);
-?> -->
 <main>
     <button class="btn btn-outline-link mt-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
@@ -27,44 +24,13 @@ var_dump($_SESSION);
                     </svg>
                     <a href="#">Home</a>
                 </div>
-
-                <?php if (!isset($_SESSION["userId"]))
-                {
-                    ?>
-                    <div class="p-3 list-inline">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#4621b0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
-                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                            <circle cx="12" cy="7" r="4"></circle>
-                        </svg>
-                        <a href="<?= SITE_URL ?>login">Entre ou Cadastre-se</a>
-                    </div>
-                    <?php
-                }
-                else
-                {
-                    if ($_SESSION["userNivel"] == 1)
-                    {
-                        ?>
-                        <div class="p-1 dropdown">
-                            <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#4621b0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-unlock">
-                                    <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-                                    <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
-                                </svg>
-                                Área Administrativa
-                            </button>
-                            <ul class="dropdown-menu text-small" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="text-light dropdown-item" href="<?= SITE_URL ?>controllerAdmin/pedidos">Pedidos</a></li>
-                                <li><a class="text-light dropdown-item" href="<?= SITE_URL ?>controlerAdmin/listUsuario">Usuários</a></li>
-                                <li><a class="text-light dropdown-item" href="<?= SITE_URL ?>controllerAdmin/listLanche">Lanches</a></li>
-                                <li><a class="text-light dropdown-item" href="<?= SITE_URL ?>controllerAdmin/listCategoria">Categorias</a></li>
-                            </ul>
-                        </div>
-                        <?php
-                    }
-                }
-                ?>
-
+                <div class="p-3 list-inline">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#4621b0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                        <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                    <a href="<?=SITE_URL?>login">Entre ou Cadastre-se</a>
+                </div>
                 <div class="p-3 list-inline">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#4621b0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart">
                         <circle cx="9" cy="21" r="1"></circle>
@@ -81,12 +47,26 @@ var_dump($_SESSION);
                         </svg>
                         Meus pedidos</a>
                 </div>
-
+                <div class="p-1 dropdown">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#4621b0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-unlock">
+                            <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                            <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
+                        </svg>
+                        Área Administrativa
+                    </button>
+                    <ul class="dropdown-menu text-small" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="text-light dropdown-item" href="<?=SITE_URL?>controllerAdmin/pedidos">Pedidos</a></li>
+                        <li><a class="text-light dropdown-item" href="<?=SITE_URL?>controlerAdmin/listUsuario">Usuários</a></li>
+                        <li><a class="text-light dropdown-item" href="<?=SITE_URL?>controllerAdmin/listLanche">Lanches</a></li>
+                        <li><a class="text-light dropdown-item" href="<?=SITE_URL?>controllerAdmin/listCategoria">Categorias</a></li>
+                    </ul>
+                </div>
                 <div class="p-3 list-inline">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#4621b0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
                         <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                     </svg>
-                    <a href="<?= SITE_URL ?>fale-conosco">Fale conosco</a>
+                    <a href="<?=SITE_URL?>fale-conosco">Fale conosco</a>
                 </div>
                 <div class="p-3 list-inline">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#4621b0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info">
@@ -96,23 +76,21 @@ var_dump($_SESSION);
                     </svg>
                     <a href="#">Sobre nós</a>
                 </div>
-                
+
                 <hr>
-                <?php if (isset($_SESSION["userId"])) : ?>
-                    <div class="dropdown perfil p-4 fixed-bottom">
-                        <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="<?= SITE_URL ?>assets/img/user.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                            <strong><?= $_SESSION['userNome'] ?></strong>
-                        </a>
-                        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-                            <li><a class="text-light dropdown-item" href="#">Perfil</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="text-light dropdown-item" href="<?= SITE_URL ?>usuario/logout">Sair</a></li>
-                        </ul>
-                    </div>
-                <?php endif; ?>
+                <div class="dropdown perfil p-4 fixed-bottom">
+                    <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                        <strong>Usuariodsfd@22</strong>
+                    </a>
+                    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                        <li><a class="text-light dropdown-item" href="#">Perfil</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="text-light dropdown-item" href="#">Sair</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
