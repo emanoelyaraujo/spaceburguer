dbf1f7fdd1e13dc20ac220af734d5f71fa1acdf2
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-10-12 00:43:33 -0300

    fix{CRUD Clientes}: correções e ícones na lista

--- lib/Formulario.php
+++ lib/Formulario.php
@@ -33,7 +33,10 @@ class Formulario
         {
             $textoBtnNovo = '
                 <a href="' . SITE_URL . '/' . $parametro['controller'] . '/form/insert" class="btn btn-secondary btn-sm btn-icons-crud" title="Novo">
-                    <i class="fa fa-plus" area-hidden="true"></i>Novo
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
                 </a>
             ';
         }
@@ -48,8 +51,15 @@ class Formulario
                                     </div>
                                     <div class="col-2 mt-5 mb-5 text-right">
                                         ' . $textoBtnNovo . '
-                                        <a href="' . SITE_URL . '/' . $parametro['controller'] . '/lista" class="btn btn-secondary btn-sm btn-icons-crud" title="Lista">
-                                            <i class="fa fa-list" area-hidden="true"></i>Lista
+                                        <a href="' . SITE_URL . '/' . $parametro['controller'] . '/lista" class="btn btn-secondary btn-sm btn-icons-crud" title="Lista">                                            
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list">
+                                                <line x1="8" y1="6" x2="21" y2="6"></line>
+                                                <line x1="8" y1="12" x2="21" y2="12"></line>
+                                                <line x1="8" y1="18" x2="21" y2="18"></line>
+                                                <line x1="3" y1="6" x2="3.01" y2="6"></line>
+                                                <line x1="3" y1="12" x2="3.01" y2="12"></line>
+                                                <line x1="3" y1="18" x2="3.01" y2="18"></line>
+                                            </svg>
                                         </a>
                                     </div>
                                 </div>';
--- site/admin/formUsuarios.php
+++ site/admin/formUsuarios.php
@@ -4,136 +4,85 @@
     "acao" => $aDados['acao']
 ]) ?>
 
+<?php
+
+$textoBotao = "";
+$corBotao = "";
+
+if ($aDados["acao"] == "insert" || $aDados["acao"] == "update")
+{
+    $textoBotao = "Gravar";
+    $corBotao = "primary";
+}
+else if ($aDados["acao"] == "delete")
+{
+    $textoBotao = "Exluir";
+    $corBotao = "danger";
+}
+
+?>
+
 <main class="container">
-
     <section class="mb-5">
-
         <form method="POST" action="<?= SITE_URL ?>usuario/<?= $aDados['acao'] ?>">
-
             <div class="row">
-
-                <div class="form-group col-12 col-md-8">
+                <div class="form-group col-12 col-md-8 mb-3">
                     <label for="nome" class="form-label">Nome</label>
-                    <input type="text" name="nome" id="nome" class="form-control" maxlength="50" 
-                    <?php 
-                        if (isset($aDados['acao']) && $aDados['acao'] == 'view' or $aDados['acao'] == 'delete'){
-                            echo ('readonly');
-                        }
-                    ?>
-                    value="<?= isset($aDados['data']['nome']) ? $aDados['data']['nome'] : "" ?>" required autofocus placeholder="Nome completo do usuário">
+                    <input type="text" name="nome" id="nome" class="form-control" maxlength="50"
+                        <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "") ?>
+                        value="<?= isset($aDados['data']['nome']) ? $aDados['data']['nome'] : "" ?>" 
+                        required autofocus placeholder="Nome completo do usuário"
+                    >
                 </div>
 
-                <div class="form-group col-12 col-md-4">
+                <div class="form-group col-12 col-md-4 mb-3">
                     <label for="status" class="form-label">Status</label>
-                    <select name="status" id="status" class="form-control"
-                    <?php 
-                        if (isset($aDados['acao']) && $aDados['acao'] == 'view' or $aDados['acao'] == 'delete'){
-                            echo ('disabled');
-                        }
-                    ?>
-                    required>
-                        <option value="" <?= isset($aDados['data']['status']) ? $aDados['data']['status'] == ""  ? "selected" : "" : "" ?>>.....</option>
-                        <option value="1" <?= isset($aDados['data']['status']) ? $aDados['data']['status'] == "1" ? "selected" : "" : "" ?>>Ativo</option>
-                        <option value="2" <?= isset($aDados['data']['status']) ? $aDados['data']['status'] == "2" ? "selected" : "" : "" ?>>Inativo</option>
+                    <select name="status" id="status" class="form-control" required
+                        <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "disabled" : "") : "") ?> >
+                        <option value="" <?= (isset($aDados['data']['status']) ? ($aDados['data']['status'] == ""  ? "selected" : "") : "") ?> >.....</option>
+                        <option value="1" <?= (isset($aDados['data']['status']) ? ($aDados['data']['status'] == "1" ? "selected" : "") : "") ?> >Ativo</option>
+                        <option value="2" <?= (isset($aDados['data']['status']) ? ($aDados['data']['status'] == "2" ? "selected" : "") : "") ?> >Inativo</option>
                     </select>
                 </div>
 
-                <div class="form-group col-12 col-md-6">
+                <div class="form-group col-12 col-md-6 mb-3">
                     <label for="email" class="form-label">E-mail</label>
                     <input type="text" name="email" id="email" class="form-control" maxlength="100" 
-                    <?php 
-                        if (isset($aDados['acao']) && $aDados['acao'] == 'view' or $aDados['acao'] == 'delete'){
-                            echo ('readonly');
-                        }
-                    ?>
-                    value="<?= isset($aDados['data']['email']) ? $aDados['data']['email'] : "" ?>" required placeholder="exemplo@dominio.com">
+                    <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "")?> 
+                        value="<?= isset($aDados['data']['email']) ? $aDados['data']['email'] : "" ?>" 
+                        required placeholder="exemplo@dominio.com"
+                    >
                 </div>
 
-                <div class="form-group col-12 col-md-3">
+                <div class="form-group col-12 col-md-3 mb-3">
                     <label for="telefone" class="form-label">Telefone</label>
-                    <input type="text" name="telefone" id="telefone" class="form-control" minlength="11"  maxlength="15" 
-                    <?php 
-                        if (isset($aDados['acao']) && $aDados['acao'] == 'view' or $aDados['acao'] == 'delete'){
-                            echo ('readonly');
-                        }
-                    ?>
-                    value="<?= isset($aDados['data']['telefone']) ? $aDados['data']['telefone'] : "" ?>" autofocus placeholder="(00) 00000-0000">
+                    <input type="text" name="telefone" id="telefone" class="form-control" minlength="11" maxlength="15"
+                        <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "")?> 
+                        value="<?= isset($aDados['data']['telefone']) ? $aDados['data']['telefone'] : "" ?>" 
+                        autofocus placeholder="(00) 00000-0000"
+                    >
                 </div>
 
-                <div class="form-group col-12 col-md-3">
+                <div class="form-group col-12 col-md-3 mb-3">
                     <label for="nivel" class="form-label">Nível</label>
-                    <select name="nivel" id="nivel" class="form-control"
-                    <?php 
-                        if (isset($aDados['acao']) && $aDados['acao'] == 'view' or $aDados['acao'] == 'delete'){
-                            echo ('disabled');
-                        }
-                    ?>
-
-                    required>
-                        <option value="" <?= isset($aDados['data']['nivel']) ? $aDados['data']['nivel'] == ""    ? "selected" : "" : "" ?>>.....</option>
-                        <option value="1" <?= isset($aDados['data']['nivel']) ? $aDados['data']['nivel'] == "1"   ? "selected" : "" : "" ?>>Administrador</option>
-                        <option value="2" <?= isset($aDados['data']['nivel']) ? $aDados['data']['nivel'] == "2"  ? "selected" : "" : "" ?>>Usuário</option>
-                        <option value="3" <?= isset($aDados['data']['nivel']) ? $aDados['data']['nivel'] == "3"  ? "selected" : "" : "" ?>>Motoboy</option>
+                    <select name="nivel" id="nivel" class="form-control" required
+                    <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "disabled" : "") : "") ?> >
+                        <option value="" <?= (isset($aDados['data']['nivel']) ? ($aDados['data']['nivel'] == ""    ? "selected" : "") : "") ?>>.....</option>
+                        <option value="1" <?= (isset($aDados['data']['nivel']) ? ($aDados['data']['nivel'] == "1"   ? "selected" : "") : "") ?>>Administrador</option>
+                        <option value="2" <?= (isset($aDados['data']['nivel']) ? ($aDados['data']['nivel'] == "2"  ? "selected" : "") : "") ?>>Usuário</option>
+                        <option value="3" <?= (isset($aDados['data']['nivel']) ? ($aDados['data']['nivel'] == "3"  ? "selected" : "") : "") ?>>Motoboy</option>
                     </select>
-                </div>
-
-                <div class="form-group col-12 col-md-6">
-                    <label for="senha" class="form-label">Senha</label>
-                    <input type="password" name="senha" id="senha" class="form-control"
-
-                    <?php 
-                        if (isset($aDados['acao']) && $aDados['acao'] == 'view' or $aDados['acao'] == 'delete'){
-                            echo ('readonly');
-                        }
-                    ?>
-                    value="<?= isset($aDados['data']['senha']) ? $aDados['data']['senha'] : "" ?>" required placeholder="Informe uma senha">
-                </div>
-
-                <div class="form-group col-12 col-md-6">
-                    <label for="confSenha" class="form-label">Conferir a senha</label>
-                    <input type="password" name="confSenha" id="confSenha" class="form-control"
-                    <?php 
-                        if (isset($aDados['acao']) && $aDados['acao'] == 'view' or $aDados['acao'] == 'delete'){
-                            echo ('readonly');
-                        }
-                    ?>
-                    value="<?= isset($aDados['data']['senha']) ? $aDados['data']['senha'] : "" ?>" required placeholder="Confirme a senha">
                 </div>
 
                 <input type="hidden" name="id" value="<?= isset($aDados['data']['id']) ? $aDados['data']['id'] : "" ?>">
 
                 <div class="form-group col-12 col-md-4 mt-2">
-
-                    <?php if ($acao != "view") : ?>
-
-                        <?php if ($acao != 'delete'){ ?>
-
-                            <button type="submit" value="submit" class="btn btn-success me-3">Gravar</button>
-
-                        <?php }else{ ?>
-
-                            <button type="submit" value="submit" class="btn btn-danger me-3">Deletar</button>
-
-                        <?php }
-
-                        endif; ?>
-
-                        <?php if ($acao != "view"){ ?>
-
-                            <a href="<?= SITE_URL ?>/Usuario/lista" class="btn btn-outline-warning">Voltar</a>
-
-                        <?php }else { ?>
-
-                            <a href="<?= SITE_URL ?>/Usuario/lista" class="btn btn-warning">Voltar</a>
-
-                        <?php } ?>
-
+                    <a href="<?= SITE_URL ?>/Usuario/lista" class="btn btn-outline-secondary">Voltar</a>
+                    <?php if($acao != "view"): ?>     
+                        <button type="submit" value="submit" class="btn btn-<?= $corBotao ?> me-3"><?= $textoBotao ?></button>
+                    <?php endif; ?>
                 </div>
-
             </div>
-
         </form>
-
     </section>
-
 </main>
