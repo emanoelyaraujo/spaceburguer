e1467cff718543ffc8ffae0df0624b4cc425d3db
emanoelyaraujo <emanoelyaraujo004@gmail.com>
2021-10-14 17:31:56 -0300

    fix{Form Lanche}: select com as categorias

--- site/admin/formLanche.php
+++ site/admin/formLanche.php
@@ -25,33 +25,31 @@ if ($aDados["acao"] == "insert" || $aDados["acao"] == "update") {
             <div class="row">
                 <div class="form-group col-md-4 mb-3">
                     <label for="id_categoria" class="form-label">Categoria</label>
-                    <select name="id_categoria" class="form-control" id="id_categoria" <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "disabled" : "") : "") ?>>
-                        <option value="">...</option>
-                        <?php 
-                        foreach ($aDados['categoria'] as $categoria)
-                        {
-                            foreach ($aDados['data'] as $data) 
-                            {
-                                var_dump($categoria, $data);
-                             ?>
-                                
-                             <?php 
-                            }
-                        }
-                        ?>
+                    <select name="id_categoria" class="form-control" id="id_categoria" 
+                        <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "disabled" : "") : "") ?>>
+                        <option value="" selected disabled></option>
+                        <?php foreach ($aDados['categoria'] as $categoria): ?>
+                            <option value="" <?= (isset($aDados["data"]) ? ($categoria["id"] == $aDados["data"]["id_categoria"] ? "selected" : "") : "") ?> > <?= $categoria["descricao"] ?></option>
+                        <?php endforeach; ?>
                     </select>
                 </div>
 
                 <div class="form-group col-lg-6 mb-3">
                     <label for="descricao" class="form-label">Descrição</label>
-                    <input type="text" name="descricao" id="descricao" class="form-control" maxlength="250" <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "") ?> value="<?= isset($aDados['data']['descricao']) ? $aDados['data']['descricao'] : "" ?>" required autofocus placeholder="X-Egge-bacon-burguer">
+                    <input type="text" name="descricao" id="descricao" class="form-control" maxlength="250" 
+                        <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "") ?> 
+                        value="<?= isset($aDados['data']['descricao']) ? $aDados['data']['descricao'] : "" ?>" 
+                        required autofocus placeholder="X-Egge-bacon-burguer"
+                    >
                 </div>
 
                 <div class="col-md-6">
                     <label for="ingredientes" class="form-label">Ingredientes</label>
-                    <textarea class="form-control" id="ingredientes" name="ingredientes" <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "") ?>>
-                    <?= isset($aDados['data']['ingredientes']) ? $aDados['data']['ingredientes'] : "" ?>
-                </textarea>
+                    <textarea class="form-control" id="ingredientes" name="ingredientes" 
+                            <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "") ?>
+                    >
+                        <?= isset($aDados['data']['ingredientes']) ? $aDados['data']['ingredientes'] : "" ?>
+                    </textarea>
                 </div>
 
                 <div class="form-group col-lg-6 mb-3">
@@ -62,12 +60,15 @@ if ($aDados["acao"] == "insert" || $aDados["acao"] == "update") {
                                 <div class="input-group-prepend">
                                     <div class="input-group-text"><strong>R$</strong></div>
                                 </div>
-                                <input type="text" name="preco" id="preco" class="form-control" maxlength="5" <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "") ?> value="<?= isset($aDados['data']['preco']) ? $aDados['data']['preco'] : "" ?>" placeholder="00,00">
+                                <input type="text" name="preco" id="preco" class="form-control" maxlength="5" 
+                                    <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "readonly" : "") : "") ?> 
+                                    value="<?= isset($aDados['data']['preco']) ? $aDados['data']['preco'] : "" ?>" placeholder="00,00">
                             </div>
                         </div>
                         <div class="col-5">
                             <label for="status" class="form-label">Status</label>
-                            <select name="status" id="status" class="form-control" required <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "disabled" : "") : "") ?>>
+                            <select name="status" id="status" class="form-control" required 
+                                <?= (isset($aDados['acao']) ? ($aDados['acao'] == 'view' || $aDados['acao'] == 'delete' ? "disabled" : "") : "") ?>>
                                 <option value="" <?= (isset($aDados['data']['status']) ? ($aDados['data']['status'] == ""  ? "selected" : "") : "") ?>>.....</option>
                                 <option value="1" <?= (isset($aDados['data']['status']) ? ($aDados['data']['status'] == "1" ? "selected" : "") : "") ?>>Ativo</option>
                                 <option value="2" <?= (isset($aDados['data']['status']) ? ($aDados['data']['status'] == "2" ? "selected" : "") : "") ?>>Inativo</option>
@@ -98,9 +99,7 @@ if ($aDados["acao"] == "insert" || $aDados["acao"] == "update") {
         });
 
     $(document).ready(function() {
-        let detalhamento = CKEDITOR.replace('ingredientes');
-
-
+        let ingredientes = CKEDITOR.replace('ingredientes');
 
         ingredientes.on('required', function(evt) {
             ingredientes.showNotification('This field is required.', 'warning');
