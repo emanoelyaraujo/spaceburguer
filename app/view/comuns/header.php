f377153fdca1e7fe42b15fc82382ecba0967e44a
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-11-05 00:08:02 -0300

    fix: retirar o toasts

--- app/view/comuns/header.php
+++ app/view/comuns/header.php
@@ -4,11 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
     <link rel="icon" href="<?= SITE_URL ?>assets/img/space-icon.png" type="image/png">
     <link rel="stylesheet" href="<?= SITE_URL ?>assets/css/bootstrap.min.css">
+    <script src="<?= SITE_URL ?>assets/js/bootstrap.bundle.js"></script>
     <link href="<?= SITE_URL ?>assets/css/style.css" rel="stylesheet">
     <script src="<?= SITE_URL ?>assets/jQuery/jquery-3.2.1.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
@@ -25,19 +23,3 @@
 </head>
 
 <body>
-    <div class="sticky-top">
-        <div class="position-relative" style="z-index: 1000; ">
-            <div class="toast-container position-absolute top-0 end-0 p-3">
-                <div class="toast" role="alert">
-                    <div class="toast-header">
-                        <img src="<?= SITE_URL ?>assets/img/SVG/x.svg" class="rounded me-2" alt="...">
-                        <strong class="me-auto">Bootstrap</strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    <div class="toast-body">
-                        See? Just like this.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
