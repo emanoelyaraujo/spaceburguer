b0d2e9e2329e2574476cc57b19c7571041ef1eb2
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-11-16 00:31:02 -0300

    feat: icones na navbar

--- app/view/comuns/navbar.php
+++ app/view/comuns/navbar.php
@@ -1,7 +1,3 @@
-<style>
-    
-</style>
-
 <main>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
@@ -19,63 +15,67 @@
             <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#"><i class="fas fa-home"></i> Home</a>
                     </li>
 
-                    <?php  
-                        if (isset($_SESSION["userId"]))
-                        {
-                            if ($_SESSION["userNivel"] == 1)
-                            {
-                                ?>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Área Administrativa
-                                        </a>
-                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                            <li><a class="dropdown-item" href="<?= SITE_URL ?>Categoria/lista">Categorias</a></li>
-                                            <li><a class="dropdown-item" href="<?= SITE_URL ?>Lanche/lista">Lanches</a></li>
-                                            <li><a class="dropdown-item" href="<?= SITE_URL ?>UsuarioAdmin/lista">Usuários</a></li>
-                                        </ul>
-                                    </li>
-                                <?php
-                            }else if($_SESSION["userNivel"] == 2)
-                            {
-                                ?>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?= SITE_URL ?>Carrinho/index">Carrinho</a>
-                                </li>
-                                <?php
-                            }
-                        }
-                    ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= SITE_URL ?>Home/faleConosco">Fale Conosco</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sobre nós</a>
-                    </li>
                     <?php
-                    if(isset($_SESSION["userId"]))
+                    if (isset($_SESSION["userId"]))
                     {
+                        if ($_SESSION["userNivel"] == 1)
+                        {
+                            ?>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fas fa-user-lock"></i> Área Administrativa
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="<?= SITE_URL ?>Categoria/lista"><i class="fas fa-tags"></i> Categorias</a></li>
+                                    <li><a class="dropdown-item" href="<?= SITE_URL ?>Lanche/lista"><i class="fas fa-hamburger"></i> Lanches</a></li>
+                                    <li><a class="dropdown-item" href="<?= SITE_URL ?>UsuarioAdmin/lista"><i class="fas fa-users-cog"></i> Usuários</a></li>
+                                </ul>
+                            </li>
+                            <?php
+                        }
+                    }
+                    ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= SITE_URL ?>Home/faleConosco"><i class="fas fa-envelope"></i> Fale Conosco</a>
+                    </li>
+
+                    <?php
+
+                    if (isset($_SESSION["userId"]))
+                    {
+                        if ($_SESSION['userNivel'] == "2")
+                        {
+                            ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= SITE_URL ?>Carrinho/index"><i class="fas fa-shopping-cart"></i> Carrinho</a>
+                            </li>
+                            <?php
+                        }
                         ?>
-                          
+
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <?= $_SESSION["userNome"] ?>
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="<?= SITE_URL ?>MinhaConta/index">Minha Conta</a></li>
+                                <li><a class="dropdown-item" href="<?= SITE_URL ?>MinhaConta/index"><i class="fas fa-user"></i> Minha Conta</a></li>
+                                <?php if($_SESSION['userNivel'] == "2"): ?>
+                                    <li><a class="dropdown-item" href="<?= SITE_URL ?>MinhaConta/informacoesPedido"><i class="fas fa-shopping-bag"></i> Meus Pedidos</a></li>
+                                <?php endif; ?>
                                 <hr>
-                                <li><a class="dropdown-item" href="<?= SITE_URL ?>Login/logout">Sair</a></li>
+                                <li><a class="dropdown-item" href="<?= SITE_URL ?>Login/logout"><i class="fas fa-sign-out-alt"></i> Sair</a></li>
                             </ul>
                         </li>
                         <?php
                     }
                     ?>
                 </ul>
- 
-                <?php if (!isset($_SESSION["userId"])): ?>
+
+                <?php if (!isset($_SESSION["userId"])) : ?>
                     <div class="d-flex">
                         <a href="<?= SITE_URL ?>Login/index" class="btn btn-sm btnRoxo">Entrar</a>
                         <a href="<?= SITE_URL ?>Login/cadastrar" class="btn btn-sm btn-outline ms-2 btnLaranja">Cadastre-se</a>
