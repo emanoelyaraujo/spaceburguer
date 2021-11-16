4d75e7938ab880cb99b72b1a126da1ea11d3cf0b
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-11-16 00:28:27 -0300

    feat: Pagamento e Informacoes JS

---
+++ assets/js/informacoesPedido.js
@@ -0,0 +1,44 @@
+$.get('/MinhaConta/getStatusPedido').done((response) => {
+    response = JSON.parse(response)
+    $.each(response.pedido, function(key, value) {
+        if (value.status == "F") {
+            $(`#finalizado_${value.id}`).addClass("active")
+        }
+        if (value.status == "T") {
+            $(`#finalizado_${value.id}`).addClass("active")
+            $(`#transporte_${value.id}`).addClass("active")
+        }
+        if (value.status == "E") {
+            $(`#finalizado_${value.id}`).addClass("active")
+            $(`#transporte_${value.id}`).addClass("active")
+            $(`#entregue_${value.id}`).addClass("active")
+        }
+    });
+})
+
+function abreModal(id) {
+    var html = '';
+    $.get(`/MinhaConta/getItens/${id}`).done((response) => {
+        response = JSON.parse(response)
+        $.each(response.itens, function(key, value) {
+            html += `
+                <div class='card p-4 mt-2'>
+                    <div class="row">
+                        <div class="col-5 col-sm-6">
+                            <img src="${value.imagem}" class="img-fluid rounded-center" width="142" height="112" alt="...">
+                        </div>
+                        <div class="col-7 col-sm-6 mt-3">
+                            <h6>${value.descricao}</h6>
+                            <p>Valor Total: <strong>R$ ${value.valor_total.replace('.', ',')}</strong></p>
+                            <p>Quantidade: <strong>${value.quantidade}</strong></p>
+                        </div>
+                    </div>
+                </div>
+            `;
+            $(".modal-title").html(`Itens do Pedido ${value.id}`)
+        });
+        var myModal = new bootstrap.Modal(document.getElementById("exampleModal"))
+        $(".modal-body").html(html)
+        myModal.show()
+    })
+}
---
+++ assets/js/pagamento.js
@@ -0,0 +1,97 @@
+let deliverySelecionado = true
+
+$(".tabPagamento").on("click", function () {
+    if (deliverySelecionado && $("a[href='#tabDelivery']").hasClass("active")) {
+        return
+    }
+
+    if ($(this).attr('href') == "#tabRetirada") {
+        acao = "-"
+        $("#labelPagamento").html("Pague na retirada")
+        $("#informacoesPedido").html("")
+        $("#dadosCartao").html("")
+        $.get(`/Pagamento/removeEnderecoCartao`)
+    } else {
+        acao = "+"
+        $("#labelPagamento").html("Pague na entrega")
+    }
+
+    $.post("/Pagamento/frete", {
+        acao
+    }).done(function (response) {
+        response = JSON.parse(response)
+        document.getElementById('frete').innerHTML = `R$ ${response.frete.replace('.', ',')}`
+        document.getElementById('total').innerHTML = `R$ ${response.total.replace('.', ',')}`
+    });
+
+    if (!deliverySelecionado && $("a[href='#tabDelivery']").hasClass("active")) {
+        deliverySelecionado = true
+    } else {
+        deliverySelecionado = false
+    }
+});
+
+function chamaModal() {
+    var myModal = new bootstrap.Modal(document.getElementById('modalEndereco'))
+    myModal.show()
+}
+
+function chamaView(id) {
+
+    $.post("/MinhaConta/setPill", {
+        id: id
+    });
+
+    window.location = "MinhaConta/index"
+}
+
+function addEndereco() {
+
+    $('#modalEndereco').modal('hide');
+
+    idEndereco = $("input[name=endereco]:checked").val()
+
+    $.post("/Pagamento/addEndereco", {
+        idEndereco
+    }).done(function (response) {
+        response = JSON.parse(response)
+        document.getElementById('informacoesPedido').innerHTML = ` ${response.rua}, ${response.numero}<br>${response.bairro}, ${response.cep}`
+    });
+}
+
+function addCartao() {
+
+    $('#modalCartao').modal('hide');
+
+    idCartao = $("input[name=cartao]:checked").val()
+
+    $.post("/Pagamento/addCartao", {
+        idCartao
+    }).done(function (response) {
+        response = JSON.parse(response)
+        document.getElementById('dadosCartao').innerHTML = ` ${response.nomeCartao}<br>${response.numero}`
+    });
+}
+
+$("#pagamento").on('change', function () {
+
+    metodo = $("#pagamento").val()
+    $.post("/Pagamento/metodoPag", {
+        metodo
+    });
+
+    if ($("#pagamento").val() == "C" && $("a[href='#tabDelivery']").hasClass("active")) {
+
+        var myModal = new bootstrap.Modal(document.getElementById('modalCartao'))
+        myModal.show()
+    }
+})
+
+function chamaViewCartao() {
+
+    $.post("/MinhaConta/setPill", {
+        id: "#v-pills-cartao"
+    });
+
+    window.location = "MinhaConta/index"
+}
