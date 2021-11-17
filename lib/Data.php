895c788181d1181e8d3765392634ee17e24e8a13
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-11-17 00:36:02 -0300

    feat{Data}:função que calcula diferença de dias

--- lib/Data.php
+++ lib/Data.php
@@ -2,7 +2,7 @@
 
 class Data
 {
-    
+
     /**
      * formata a data para o formato d/m/Y
      *
@@ -14,7 +14,7 @@ class Data
     {
         return (!empty($date) ? date($type == 1 ? 'd/m/Y' : 'd/m/Y H:i:s', strtotime($date)) : '');
     }
-    
+
     /**
      * retorna data e hora atual
      *
@@ -25,5 +25,23 @@ class Data
         date_default_timezone_set("America/Sao_Paulo");
 
         return date('Y-m-d H:i:s', time());
+    }
+    
+    /**
+     * diferencaData
+     *
+     * @param  string $data_inicio
+     * @param  string $data_fim
+     * @return int
+     */
+    public static function diferencaData($data_inicio, $data_fim = '')
+    {
+        $data_inicio = new DateTime($data_inicio);
+        $data_fim = new DateTime(empty($data_fim) ? date("Y-m-d H:i:s") : $data_fim);
+        
+        // Resgata diferença entre as datas
+        $dateInterval = $data_inicio->diff($data_fim);
+
+        return $dateInterval->days;
     }
 }
