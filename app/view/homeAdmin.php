3d6bf800c5f32db2fee93d1e2196d9b881371b94
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-11-22 10:31:27 -0300

    fix{home adm}: retirar html e usar métodos da lib Tabela

--- app/view/homeAdmin.php
+++ app/view/homeAdmin.php
@@ -3,99 +3,40 @@
 $tabFinalizados = '';
 $tabACaminho = '';
 $tabEntregue = '';
-$totalFinalizados = 0;
-$totalACaminho = 0;
-$totalEntregues = 0;
+$total = [
+    "totalFinalizados" => 0,
+    "totalACaminho" => 0,
+    "totalEntregues" => 0
+];
 
 foreach ($pedidos['pedidos'] as $key => $p)
 {
     if ($p["status"] == 'F')
     {
-        $totalFinalizados += 1;
-        $tabFinalizados .= Lista::tbodyPedidos($p, $pedidos['entregadores']);
+        $total['totalFinalizados'] += 1;
+        $tabFinalizados .= Tabela::tbodyPedidos($p, $pedidos['entregadores']);
     }
 
     if ($p["status"] == 'C')
     {
-        $totalACaminho += 1;
-        $tabACaminho .= Lista::tbodyPedidos($p, $pedidos['entregadores']);
+        $total['totalACaminho'] += 1;
+        $tabACaminho .= Tabela::tbodyPedidos($p, $pedidos['entregadores']);
     }
 
     if ($p["status"] == 'E')
     {
-        $totalEntregues += 1;
-        $tabEntregue .= Lista::tbodyPedidos($p, $pedidos['entregadores']);
+        $total['totalEntregues'] += 1;
+        $tabEntregue .= Tabela::tbodyPedidos($p, $pedidos['entregadores']);
     }
 }
 
-echo Formulario::exibeMsgError() . Formulario::exibeMsgSucesso() 
+echo Formulario::exibeMsgError() . Formulario::exibeMsgSucesso();
+
+echo Tabela::tabelaHome($total, $tabFinalizados, $tabACaminho, $tabEntregue);
+
+echo Tabela::modelItensPedido()
+
 ?>
-<div class="container mt-3 p-3 mb-4">
-    <h1 style="color: #433A8F;" class="">Olá, administrador!</h1>
-    <h2>Bem-vindo a Home Admin!</h2>
-    <nav class="mt-5 mb-4">
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">
-                Finalizados <span class="badge bg-danger"><?= $totalFinalizados ?></span>
-            </button>
-            <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">
-                Á caminho <span class="badge bg-primary"><?= $totalACaminho ?></span>
-            </button>
-            <button class="nav-link" id="nav-fin-tab" data-bs-toggle="tab" data-bs-target="#nav-fin" type="button" role="tab" aria-controls="nav-fin" aria-selected="false">
-                Entregues <span class="badge bg-success"><?= $totalEntregues ?></span>
-            </button>
-        </div>
-    </nav>
-    <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-            <div class="table-responsive">
-                <table class="table table-hover table-bordered table-light table-sm tblLista">
-                    <?= Lista::theaderPedidos() ?>
-                    <tbody>
-                        <?= $tabFinalizados ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-            <div class="table-responsive">
-                <table class="table table-hover table-bordered table-light table-sm tblLista">
-                    <?= Lista::theaderPedidos() ?>
-                    <tbody>
-                        <?= $tabACaminho ?>
-                    </tbody>
-                </table>
-            </div>
-
-        </div>
-        <div class="tab-pane fade" id="nav-fin" role="tabpanel" aria-labelledby="nav-fin-tab">
-            <div class="table-responsive">
-                <table class="table table-hover table-bordered table-light table-sm tblLista">
-                    <?= Lista::theaderPedidos() ?>
-                    <tbody>
-                        <?= $tabEntregue ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Modal Itens -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Modal -->
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
