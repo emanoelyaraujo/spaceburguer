613cade399b8668f30f391191eaf94d485917f1e
RomarioOliveira21 <87107730+RomarioOliveira21@users.noreply.github.com>
2021-11-22 19:36:48 -0300

    feat - view sobre nos

---
+++ app/view/sobre-nos.php
@@ -0,0 +1,16 @@
+<div class="container mt-5 mb-5 text-light d-flex justify-content-center p-3">
+    <div class="row align-items-center">
+        <div class="col-md-6 col-lg-8 p-5" id="circle-radius">
+            <h6 class="p-2 text-light">
+                O Space Burguer Delivery nasceu dos esforços de um quarteto muito unido, são anos de experiência
+                aplicados das melhores formas possíveis, garantindo uma ótima navegação, intuitividade e beleza,
+                além de um design moderno, com produtos de excelente qualidade feitos para gostos exigentes,
+                nossos lanches de outro mundo vão te teletransportar para o universo dos sabores.
+                Aproveite o melhor de nossas ofertas com entrega rápida e garantia de satisfação!
+            </h6>
+        </div>
+        <div class="col-sm dx-img">
+            <img src="../../assets/img/sobrenos.png" style="width: 300px;" alt="mascote space burguer">
+        </div>
+    </div>
+</div>
