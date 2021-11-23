cdbdf5e7fa4317c30d821061efcc43b29c5a1865
Emanoely Araújo <85528024+emanoelyaraujo@users.noreply.github.com>
2021-11-22 23:38:43 -0300

    fix{view erro}: src da imagem

--- app/view/comuns/error.php
+++ app/view/comuns/error.php
@@ -1,10 +1,10 @@
 <div class="container p-5">
    <div class="row justify-content-center">
        <div class="col-4">
-         <h3> <strong>Erro!</strong> Página não econtrada.</h3>  
+         <h3> <strong>Erro!</strong> Página não encontrada.</h3>  
        </div>
        <div class="col">
-           <img src="../../../assets/img/error.png" alt="imagem de erro 404" width="400px">
+           <img src="<?= SITE_URL ?>assets/img/error.png" alt="imagem de erro 404" width="300px">
        </div>
    </div>
 </div>
